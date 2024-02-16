ed545c532bd92ae73ec78e2f099084b7e12229bb
gtompel <92447644+gtompel@users.noreply.github.com>
2024-02-16 12:49:16 +0000

    d

--- index.js
+++ index.js
@@ -1,16 +1,3 @@
-const mimeTypes = {
-  '.html': 'text/html',
-  '.js': 'text/javascript',
-  '.css': 'text/css',
-  '.ico': 'image/x-icon',
-  '.png': 'image/png',
-  '.jpg': 'image/jpeg',
-  '.gif': 'image/gif',
-  '.svg': 'image/svg+xml',
-  '.json': 'application/json',
-  '.woff': 'font/woff',
-  '.woff2': 'font/woff2'
-}
 const express = require('express')
 const app = express()
 const port = 3000
