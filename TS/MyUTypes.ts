b70c7e66618bbe38245af29af62b237ffa19c62c
gtompel <92447644+gtompel@users.noreply.github.com>
2024-02-29 15:53:43 +0000

    com

--- TS/MyUTypes.ts
+++ TS/MyUTypes.ts
@@ -1,4 +1,6 @@
 type Nullable<T> = T | null;
-
+/* Nullable<T> - создаёт тип,
+который делает все свойства из первоначального типа T может быть либо T, либо null:
+*/
 // Пример использования
 type MaybeString = Nullable<string>; // string | null
